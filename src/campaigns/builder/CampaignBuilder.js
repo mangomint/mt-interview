b08b2708facb60e514120068de50e9d47f5f3688
sandraH <sandra_huber_@gmx.at>
2024-02-12 11:19:40 -0800

    added basic navigation

--- src/campaigns/builder/CampaignBuilder.js
+++ src/campaigns/builder/CampaignBuilder.js
@@ -1,9 +1,28 @@
 import "./CampaignBuilder.scss";
 
 function CampaignBuilder() {
+  const CampaignBuilderNav = () => (
+    <nav className="campaignNav">
+      <span className="back">
+        <span className="backArrow">&lt;</span>
+        Back to Campaigns
+      </span>
+      <div className="tabs">
+        <span className="active">1. Settings</span>
+        <span>2. Content</span>
+        <span>3. Review &amp; Send</span>
+      </div>
+      <button className="save">Save</button>
+      <button className="saveAndNext">Save &amp; Next</button>
+    </nav>
+  );
+
   return (
     <div className="campaignBuilderPage">
-      <div className="placeholder">Add your code here</div>
+      <CampaignBuilderNav />
+      <div className="placeholder">
+        Finish implementations based on the design/instructions
+      </div>
     </div>
   );
 }
